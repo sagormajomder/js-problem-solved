544a840591463df344dada7b23ba391da00a7a1d
Sagor Majomder <sagormajomder@gmail.com>
2026-01-08 00:57:38 +0600

    add arrEven function to return all even numbers from a given array

---
+++ 07-array-even.js
@@ -0,0 +1,19 @@
+// Write a function that returns all even numbers from a given array.
+
+// Example:
+
+// Input: [1, 2, 3, 4, 5, 6]
+// Output: [2, 4, 6]
+'use strict';
+
+function arrEven(arr) {
+  return arr.reduce((acc, val) => {
+    if (val % 2 === 0) {
+      acc.push(val);
+    }
+
+    return acc;
+  }, []);
+}
+
+console.log(arrEven([1, 2, 3, 4, 5, 6]));
