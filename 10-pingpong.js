5e25bd909aac5922b3157fe16de530a47b68ab33
Sagor Majomder <sagormajomder@gmail.com>
2026-01-08 01:14:40 +0600

    add pingPong function to print numbers from 1 to 20 with custom output for multiples of 3 and 5

---
+++ 10-pingpong.js
@@ -0,0 +1,23 @@
+// Write a function that prints numbers from 1 to 20.
+
+// Example:
+
+// For multiples of 3, print "Ping"
+// For multiples of 5, print "Pong"
+// For multiples of both 3 and 5, print "PingPong"
+// If the number is not a multiple of 3 or 5, print the number itself
+// Example Output:
+
+// 1, 2, Ping, 4, Pong, Ping, 7, 8, Ping, Pong, 11, Ping, 13, 14, PingPong, 16 …..
+
+'use strict';
+
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log('PingPong');
+    else if (i % 3 === 0) console.log('Ping');
+    else if (i % 5 === 0) console.log('Pong');
+    else console.log(i);
+  }
+}
+pingPong();
