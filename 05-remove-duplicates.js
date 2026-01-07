d3e09c50edf12f5f061532bb081f60d3eccea657
Sagor Majomder <sagormajomder@gmail.com>
2026-01-08 00:49:52 +0600

    add removeDuplicate function to eliminate duplicate numbers from an array

---
+++ 05-remove-duplicates.js
@@ -0,0 +1,20 @@
+// Write a function that removes all duplicate numbers from an array.
+
+// Example:
+
+// Input: [1, 2, 2, 3, 4, 4]
+// Output: [1, 2, 3, 4]
+
+'use strict';
+
+function removeDuplicate(arr) {
+  return arr.reduce((acc, val, i, arr) => {
+    if (arr[i] === arr[i + 1] && i !== arr.length - 1) return acc;
+
+    acc.push(arr[i]);
+
+    return acc;
+  }, []);
+}
+
+console.log(removeDuplicate([1, 2, 2, 3, 4, 4]));
