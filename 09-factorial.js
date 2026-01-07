6b277372bd791097dd212b656e13c648203c060d
Sagor Majomder <sagormajomder@gmail.com>
2026-01-08 01:08:20 +0600

    add factorial function to calculate the factorial of a number using recursion

---
+++ 09-factorial.js
@@ -0,0 +1,14 @@
+// Write a function that calculates the factorial of a number using a loop.
+
+// Example:
+
+// Input: 5
+// Output: 120
+
+function fact(num) {
+  if (num === 0) return 1;
+
+  return num * fact(num - 1);
+}
+
+console.log(fact(5));
