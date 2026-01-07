a367ba04accd0e6754834041887a088f9f71ceda
Sagor Majomder <sagormajomder@gmail.com>
2026-01-08 00:54:15 +0600

    add arrSum function to calculate the sum of all numbers in an array

---
+++ 06-array-sum.js
@@ -0,0 +1,13 @@
+// Write a function that returns the sum of all numbers in an array.
+
+// Example:
+
+// Input: [1, 2, 3, 4]
+// Output: 10
+
+'use strict';
+function arrSum(arr) {
+  return arr.reduce((acc, val) => (acc += val), 0);
+}
+
+console.log(arrSum([1, 2, 3, 4]));
