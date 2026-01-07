11237b7559599478cbb531042aa141433e1ef4cc
Sagor Majomder <sagormajomder@gmail.com>
2026-01-08 00:35:39 +0600

    add checkPalindrome function to determine if a string is a palindrome

---
+++ 03-palindrome.js
@@ -0,0 +1,23 @@
+// Write a function that checks if a string is a palindrome (reads the same forward and backward).
+
+// Example:
+
+// Input: "madam"
+// Output: true
+// Input: "hello"
+// Output: false
+
+function checkPalindrome(str) {
+  let count = 0;
+  for (let i = 0, j = str.length - 1; i < str.length, j >= 0; i++, j--) {
+    if (str[i] === str[j]) {
+      count++;
+    }
+  }
+
+  if (count === str.length) return true;
+  return false;
+}
+
+console.log(checkPalindrome('madam'));
+console.log(checkPalindrome('hello'));
